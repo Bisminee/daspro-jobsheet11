1da2da3adb5a850a6de22d5bfc316ab0ac34b249
Bisminee <fiksilmiah@gmail.com>
2024-11-25 08:10:19 +0700

    Fungsi pertama

--- CafeMariana.java
+++ CafeMariana.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 public class CafeMariana {
-    public static void inputPenjualan(int jumlahMenu[], int hari, String namaMenu[], int penjualan[][], double jualTinggi, String menuJualTinggi, double rataPenjualan[]){
+   
+    public static String inputPenjualan(int jumlahMenu[], int hari, String namaMenu[], int penjualan[][], int jualTinggi, String menuJualTinggi, double rataPenjualan[]){
         Scanner sc = new Scanner(System.in);
         for(int i = 0; i < namaMenu.length; i++){
             for (int j = 0; j < hari; j++) {
@@ -8,15 +9,17 @@ public class CafeMariana {
                 penjualan[i][j] = sc.nextInt();
                 jumlahMenu[i] += penjualan[i][j];
             }
-        if(jualTinggi > jumlahMenu[i]){
+        if(jualTinggi < jumlahMenu[i]){
             jualTinggi = jumlahMenu[i];
             menuJualTinggi = namaMenu[i];
         }
         rataPenjualan[i] =(jumlahMenu[i])/7; 
         }
+        return menuJualTinggi;
     }
     
-    public static void tampilanPenjualan(int hari, String namaMenu[], int penjualan[][], double rataPenjualan[], String menuJualTinggi){
+    public static void tampilanPenjualan(int jumlahMenu[], int hari, String namaMenu[], int penjualan[][], int jualTinggi, String menuJualTinggi, double rataPenjualan[]){
+        menuJualTinggi = inputPenjualan(jumlahMenu, hari, namaMenu, penjualan, jualTinggi, menuJualTinggi, rataPenjualan);
         for(int i = 0; i < hari; i++){
             System.out.println("Penjualan hari ke-" + (i+1) + ": ");
             for (int j = 0; j < namaMenu.length; j++) {
@@ -30,8 +33,8 @@ public class CafeMariana {
     }
     
     public static void main(String[] args) {
-        int penjualan[][], hari = 7, jumlahMenu[];
-        double rataPenjualan[], jualTinggi = 0;
+        int jualTinggi = 0, penjualan[][], hari = 7, jumlahMenu[];
+        double rataPenjualan[];
         String menuJualTinggi = null;
 
         String namaMenu[] = {"Kopi", "Teh", "Es Degan", "Roti Bakar", "Gorengan"};
@@ -39,7 +42,6 @@ public class CafeMariana {
         jumlahMenu = new int [namaMenu.length];
         rataPenjualan = new double[namaMenu.length];
 
-        inputPenjualan(jumlahMenu, hari, namaMenu, penjualan, jualTinggi, menuJualTinggi, rataPenjualan);
-        tampilanPenjualan(hari, namaMenu, penjualan, rataPenjualan, menuJualTinggi);
+        tampilanPenjualan(jumlahMenu, hari, namaMenu, penjualan, jualTinggi, menuJualTinggi, rataPenjualan);
     }
 }
