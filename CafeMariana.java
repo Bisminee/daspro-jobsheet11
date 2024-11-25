24597b3b04f48bbd9103d1828118166ef70013b2
Bisminee <fiksilmiah@gmail.com>
2024-11-25 08:17:07 +0700

    Fungsi pertama

--- CafeMariana.java
+++ CafeMariana.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class CafeMariana {
    
-    public static String inputPenjualan(int jumlahMenu[], int hari, String namaMenu[], int penjualan[][], int jualTinggi, String menuJualTinggi, double rataPenjualan[]){
+    public static String inputPenjualan(int jumlahMenu[], int hari, String namaMenu[], int penjualan[][], int jualTinggi[], String menuJualTinggi, double rataPenjualan[]){
         Scanner sc = new Scanner(System.in);
         for(int i = 0; i < namaMenu.length; i++){
             for (int j = 0; j < hari; j++) {
@@ -9,8 +9,8 @@ public class CafeMariana {
                 penjualan[i][j] = sc.nextInt();
                 jumlahMenu[i] += penjualan[i][j];
             }
-        if(jualTinggi < jumlahMenu[i]){
-            jualTinggi = jumlahMenu[i];
+        if(jualTinggi[0] < jumlahMenu[i]){
+            jualTinggi[0] = jumlahMenu[i];
             menuJualTinggi = namaMenu[i];
         }
         rataPenjualan[i] =(jumlahMenu[i])/7; 
@@ -18,7 +18,7 @@ public class CafeMariana {
         return menuJualTinggi;
     }
     
-    public static void tampilanPenjualan(int jumlahMenu[], int hari, String namaMenu[], int penjualan[][], int jualTinggi, String menuJualTinggi, double rataPenjualan[]){
+    public static void tampilanPenjualan(int jumlahMenu[], int hari, String namaMenu[], int penjualan[][], int jualTinggi[], String menuJualTinggi, double rataPenjualan[]){
         menuJualTinggi = inputPenjualan(jumlahMenu, hari, namaMenu, penjualan, jualTinggi, menuJualTinggi, rataPenjualan);
         for(int i = 0; i < hari; i++){
             System.out.println("Penjualan hari ke-" + (i+1) + ": ");
@@ -29,11 +29,11 @@ public class CafeMariana {
         for(int i = 0; i < namaMenu.length; i++){
             System.out.println("Rata-rata penjualan menu " + namaMenu[i] + ": " + rataPenjualan[i]);
         }
-        System.out.println("Menu dengan penjualan tertinggi: " + menuJualTinggi);
+        System.out.println("Menu dengan penjualan tertinggi: " + menuJualTinggi + "\nTerjual dengan jumlah: " +  jualTinggi[0]);
     }
     
     public static void main(String[] args) {
-        int jualTinggi = 0, penjualan[][], hari = 7, jumlahMenu[];
+        int jualTinggi[] = {0}, penjualan[][], hari = 7, jumlahMenu[];
         double rataPenjualan[];
         String menuJualTinggi = null;
 
